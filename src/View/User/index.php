ac7f8c104fd2e628280ca40fff24c7bb333863c1
General Baguettson <mathieu@bigare.eu>
2022-03-28 12:46:25 +0200

    [STEP] Added first step to the projet. Simple index and login pages to test the server

---
+++ src/View/User/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<HTML>
+
+<HEAD>
+    <TITLE>Hello I am an index</TITLE>
+    <STYLE></STYLE>
+</HEAD>
+
+<BODY>
+    <pre>
+        <?php
+            print_r($_POST);
+            print_r($_GET);
+            print_r($_SERVER);
+        ?>
+    </pre>
+</BODY>
+
+</HTML>
---
+++ src/View/User/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<HTML>
+
+<HEAD>
+    <TITLE>Zougzoug</TITLE>
+    <STYLE></STYLE>
+</HEAD>
+
+<BODY>
+    <form action="./index.php" method="POST">
+        <div>
+            <label for="name">Who dis ?: </label>
+            <input type="text" name="name" id="name" required>
+        </div>
+        <div>
+            <label for="email">Enter your email: </label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div>
+            <input type="submit" value="I am submit button">
+        </div>
+    </form>
+</BODY>
+
+</HTML>
